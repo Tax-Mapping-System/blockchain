8a19504c397553fb126632a1d8b65a9fc9eb2ca3
Janindu-Pathirana <78893460+Janindu-Pathirana@users.noreply.github.com>
2023-08-03 21:54:09 +0530

    update config

--- truffle-config.js
+++ truffle-config.js
@@ -41,11 +41,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -73,9 +73,15 @@ module.exports = {
     // }
   },
 
+  plugins: ["solidity-coverage"],
+
   // Set default mocha options here, use special reporters etc.
   mocha: {
-    // timeout: 100000
+    reporter: "eth-gas-reporter",
+    reporterOptions: {
+      // url: `https://sepolia.infura.io/v3/${infura_api_key}`,
+      excludeContracts: ["Migrations"],
+    },
   },
 
   // Configure your compilers
