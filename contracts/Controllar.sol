ea5a270aa1b4f35c0c075732b1ced573812d2cb8
Janindu-Pathirana <78893460+Janindu-Pathirana@users.noreply.github.com>
2023-08-06 00:33:58 +0530

    add role base user auth -set user -get user

--- contracts/Controllar.sol
+++ contracts/Controllar.sol
@@ -1,7 +1,53 @@
-// SPDX-License-Identifier: MIT 
-pragma solidity 0.8.21;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
+enum Role {
+    Default, //0
+    Admin, // 1
+    User // 2
+}
 
-contract Controllar{
-    
+contract Controllar {
+    address private immutable govAddress = msg.sender;
+
+    mapping(address => Role) private userRoleMap;
+
+    //events
+    event addNewUserAddress(address indexed user, Role indexed role);
+
+    //modifires
+
+    modifier onlySecondaryAuth() {
+        require(
+            requireGovAddress() || userRoleMap[msg.sender] == Role.Admin,
+            "You havent authority to access ERROR:1"
+        );
+
+        _;
+    }
+
+    modifier onlyGov() {
+        require(requireGovAddress(), "You havent authority to access ERROR:0");
+        _;
+    }
+
+    //internal
+    function requireGovAddress() internal view returns (bool) {
+        return (msg.sender == govAddress);
+    }
+
+    //external
+    function getUserRole() external view returns (Role) {
+        return (userRoleMap[msg.sender]);
+    }
+
+    function setUserRole(
+        address newUserAddress,
+        Role role
+    ) external onlySecondaryAuth {
+
+        userRoleMap[newUserAddress] = role;
+
+        emit addNewUserAddress(newUserAddress, role);
+    }
 }
