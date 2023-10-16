03892c91e08689c8df2ce3c52e9fad0753ea11e9
Janindu-Pathirana <78893460+Janindu-Pathirana@users.noreply.github.com>
2023-10-16 10:52:05 +0530

    add moeny request functions

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,11 +4,15 @@ pragma solidity ^0.8.17;
 import "./interface/IToken.sol";
 import "./utillContracts/Auth.sol";
 
+import "./interface/IController.sol";
+
 contract Token is IToken, Auth{
 
     bytes private immutableData;
     string private projectName;
     uint256 public immutable id;
+
+    address private immutable controllerContractAddress = msg.sender;
 
 
     constructor(bytes memory projectImmutableData, string memory name, uint256 tokenId)Auth()
@@ -18,15 +22,21 @@ contract Token is IToken, Auth{
         id = tokenId;
     }
 
-
+    
+    //public 
+    function getProjectName() public view returns(string memory){
+            return projectName;
+    }
 
     //external 
     function getImutableData() external view returns(bytes memory){
         return immutableData;
     }
 
-    function getProjectName() public view returns(string memory){
-        return projectName;
+    function moneyRequest(uint256 money) external onlyProjectAuth {
+        IController(controllerContractAddress).projectMoneyRequest(money, id);
+
+        emit ProjectEvent(ProjectMoneyEventType.SendMoneyRequest,money);
     }
  
 }
